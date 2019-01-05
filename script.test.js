c135792780bcd6aed03786b2bf249618fb1aac14
rayGreycloud <rayGreycloud@gmail.com>
2019-01-05 06:49:24 -0800

    basic tests of simple function

--- script.test.js
+++ script.test.js
@@ -2,9 +2,33 @@ const googleSearch = require('./script');
 
 const dbMock = ['dogs.com', 'cheesepuff.com', 'disney.com', 'dogpictures.com'];
 
-it('should return results including search term', () => {
-  const searchTerm = 'dog';
-  const results = googleSearch(searchTerm, dbMock);
-  expect(results.length).toBe(2);
-  expect(results).toContain('dogs.com');
+let searchTerm, results;
+describe('googleSearch', () => {
+  it('should return results including search term', () => {
+    searchTerm = 'test';
+    results = googleSearch(searchTerm, dbMock);
+    expect(results.length).toBe(0);
+    expect(results).toEqual([]);
+
+    searchTerm = 'dog';
+    results = googleSearch(searchTerm, dbMock);
+    expect(results.length).toBe(2);
+    expect(results).toContain('dogs.com');
+  });
+
+  it('should work with undefined and null input', () => {
+    searchTerm = undefined;
+    results = googleSearch(searchTerm, dbMock);
+    expect(results).toEqual([]);
+
+    searchTerm = null;
+    results = googleSearch(searchTerm, dbMock);
+    expect(results).toEqual([]);
+  });
+
+  it('should not return more than 3 matches', () => {
+    searchTerm = '.com';
+    results = googleSearch(searchTerm, dbMock);
+    expect(results.length).toEqual(3);
+  });
 });
