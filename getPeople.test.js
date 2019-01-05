e8587262a8229ee5cc341adcb5317b32cfcb287f
rayGreycloud <rayGreycloud@gmail.com>
2019-01-05 12:29:34 -0800

    Add tests using mocked api

--- getPeople.test.js
+++ getPeople.test.js
@@ -8,6 +8,26 @@ describe('getPeople', () => {
     const data = await getPeople(fetch);
     expect(data.count).toEqual(87);
   });
+
+  it('should return count and results', () => {
+    const mockFetch = jest.fn().mockReturnValue(
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            count: 87,
+            results: [0, 1, 2, 3, 4, 5],
+          }),
+      }),
+    );
+
+    expect.assertions(3);
+
+    return getPeople(mockFetch).then(data => {
+      expect(data.count).toEqual(87);
+      expect(mockFetch.mock.calls.length).toEqual(1);
+      expect(mockFetch).toBeCalledWith('https://swapi.co/api/people');
+    });
+  });
 });
 
 describe('getPeoplePromise', () => {
